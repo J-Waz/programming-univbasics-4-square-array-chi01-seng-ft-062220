b2708567404db1bff9d14c4314f8cb9eb7a033c9
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-14 21:06:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,7 +6,12 @@
 #square_array(new_numbers)
 #=> [1, 4, 9]
 
-def square_array (array)
-  
-  
+def square_array(array)
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
 end
